a5cfacebb352ff8ffffead7290225c68e5b6273e
vorontsov <vorontsov@nextexit.ru>
2020-11-11 22:34:42 +0300

    Initial Commit

---
+++ PhoneFormat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhoneFormat
+//
+//  Created by ANDREY VORONTSOV on 11.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
